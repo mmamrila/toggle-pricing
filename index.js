d8258dc65a7c4d4fd3135549861f26ace847de3d
Mike Mamrila <mmamrila@gmail.com>
2022-03-03 22:10:23 -0700

    attempts to get checkbox to work

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-let monthly = document.querySelector('.price-toggler .monthly');
-let yearly = document.querySelector('.price-toggler .yearly');
-let monthAmount = document.querySelectorAll('')
+let toggle = document.querySelectorAll('.togglebtn');
+let monthAmount = document.querySelectorAll('.price-monthly');
+let yearAmount = document.querySelectorAll('.price-yearly');
+
+function toggled() {
+  if (toggle.checked == true) {
+    console.log('true');
+    // yearAmount.style.display = 'block';
+  } else {
+    console.log('false');
+  }
+}
