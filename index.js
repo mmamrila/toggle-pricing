0bdea5523ca7d9e18af2afbfd361341ee939469f
Mike Mamrila <mmamrila@gmail.com>
2022-03-04 00:34:41 -0700

    changed checked to onclick

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-let toggle = document.querySelectorAll('.togglebtn');
+let toggle = document.querySelector('.togglebtn');
 let monthAmount = document.querySelectorAll('.price-monthly');
 let yearAmount = document.querySelectorAll('.price-yearly');
 
-function toggled() {
-  if (toggle.checked == true) {
-    console.log('true');
-    // yearAmount.style.display = 'block';
-  } else {
-    console.log('false');
-  }
+// function toggled() {
+//   if (toggle.checked == true) {
+//     console.log('true');
+//     // yearAmount.style.display = 'block';
+//   } else {
+//     console.log('false');
+//   }
+// }
+
+toggle.onclick = function () {
+  console.log('clicked');
 }
