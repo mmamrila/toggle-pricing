7ff727202e9c5c3bc9e36099bfe800f8bf081759
Mike Mamrila <mmamrila@gmail.com>
2022-03-04 19:18:29 -0700

    working toggle button

--- index.js
+++ index.js
@@ -3,37 +3,11 @@ let monthAmount = document.querySelectorAll('.price-monthly');
 let yearAmount = document.querySelectorAll('.price-yearly');
 
 toggle.addEventListener('click', function () {
-  console.log(toggle);
-  for (let i = 0; i < monthAmount.length; i++) {
-    if (monthAmount[i].style.display === 'block') {
-      monthAmount[i].style.display = 'none'
-    } else {
-      monthAmount[i].style.display = 'block'
-    }
-  }
-
-  for (let i = 0; i < yearAmount.length; i++) {
-    if (yearAmount[i].style.display === 'none') {
-      yearAmount[i].style.display = 'block'
-    } else {
-      yearAmount[i].style.display = 'none'
-    }
+  if (!toggle.checked) {
+    yearAmount.forEach(x => x.style.display = 'none');
+    monthAmount.forEach(x => x.style.display = 'block');
+  } else {
+    monthAmount.forEach(x => x.style.display = 'none');
+    yearAmount.forEach(x => x.style.display = 'block');
   }
 })
-// toggle.onclick = function () {
-//   for (let i = 0; i < monthAmount.length; i++) {
-//     if (monthAmount[i].style.display === 'block') {
-//       monthAmount[i].style.display = 'none'
-//     } else {
-//       monthAmount[i].style.display = 'block'
-//     }
-//   }
-
-//   for (let i = 0; i < yearAmount.length; i++) {
-//     if (yearAmount[i].style.display === 'none') {
-//       yearAmount[i].style.display = 'block'
-//     } else {
-//       yearAmount[i].style.display = 'none'
-//     }
-//   }
-// }
