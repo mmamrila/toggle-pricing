27f54ecb24da0791295872835bc8dcec1291c561
Mike Mamrila <mmamrila@gmail.com>
2022-03-06 17:33:17 -0700

    toggle for monthly/annually

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 let toggle = document.querySelector('.togglebtn');
 let monthAmount = document.querySelectorAll('.price-monthly');
 let yearAmount = document.querySelectorAll('.price-yearly');
+let monthly = document.querySelector('.monthly');
+let annually = document.querySelector('.annually');
 
 toggle.addEventListener('click', function () {
   if (!toggle.checked) {
     yearAmount.forEach(x => x.style.display = 'none');
     monthAmount.forEach(x => x.style.display = 'block');
+    monthly.classList.remove('color');
+    annually.classList.remove('color');
   } else {
     monthAmount.forEach(x => x.style.display = 'none');
     yearAmount.forEach(x => x.style.display = 'block');
+    monthly.classList.add('color');
+    annually.classList.add('color');
   }
 })
